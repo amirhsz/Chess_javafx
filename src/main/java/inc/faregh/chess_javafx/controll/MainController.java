53b0fa5e36da59bd375691d91b276a2c745c98ad
Amir <amir1386amir2006@gmail.com>
2021-09-21 23:24:18 +0330

    classes generated and play button handled

--- src/main/java/inc/faregh/chess_javafx/controll/MainController.java
+++ src/main/java/inc/faregh/chess_javafx/controll/MainController.java
@@ -1,27 +1,44 @@
 package inc.faregh.chess_javafx.controll;
 
+import javafx.fxml.Initializable;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class MainController implements Initializable {
-    
+    private Parent here;
     @FXML
-    private void PlayButtonAction(ActionEvent event) {
+    private void PlayButtonAction(ActionEvent event) throws IOException {
+        Parent play = FXMLLoader.load(getClass().getResource("Play.fxml"));
+        Stage playstg = new Stage();
+        playstg.setScene(new Scene(play));
+        playstg.setTitle("Play");
+        playstg.show();
+        here.getScene().getWindow().hide();
+    }
+    @FXML
+    private void SettingButtonAction(ActionEvent event) throws IOException {
+        Parent setting = FXMLLoader.load(getClass().getResource("Setting.fxml"));
         
     }
     @FXML
-    private void SettingButtonAction(ActionEvent event) {
-        
+    private void AboutusButtonAction(ActionEvent event) throws IOException {
+        Parent aboutus = FXMLLoader.load(getClass().getResource("About_us.fxml"));
     }
-    @FXML
-    private void AboutusButtonAction(ActionEvent event) {
-        
-    }
+
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            here = FXMLLoader.load(location);
+        } catch (IOException ex) {}
+}
+    }
 }
---
+++ src/main/java/inc/faregh/chess_javafx/controll/SettingController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inc.faregh.chess_javafx.controll;
+
+/**
+ *
+ * @author Amir
+ */
+public class SettingController {
+    
+}
